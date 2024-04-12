ab393a3329704d9fe4a050517b15c87a4a5ce989
leemj9812 <dlaudwo087@gmail.com>
2024-04-12 17:39:45 +0900

    미들웨어 및 소셜 로그인 (깃허브) 진행 중~

--- components/social-login.tsx
+++ components/social-login.tsx
@@ -8,16 +8,7 @@ export default function SocialLogin() {
       <div className="flex flex-col gap-3">
         <Link
           className="primary-btn flex h-10 items-center justify-center gap-2"
-          href="/sms"
-        >
-          <span>
-            <ChatBubbleOvalLeftEllipsisIcon className="h-6 w-6" />
-          </span>
-          <span>Continue with SMS</span>
-        </Link>
-        <Link
-          className="primary-btn flex h-10 items-center justify-center gap-2"
-          href="/githun/start"
+          href="/github/start"
         >
           <span>
             <svg
@@ -31,6 +22,15 @@ export default function SocialLogin() {
             </svg>
           </span>
           <span>Continue with Github</span>
+        </Link>
+        <Link
+          className="primary-btn flex h-10 items-center justify-center gap-2"
+          href="/sms"
+        >
+          <span>
+            <ChatBubbleOvalLeftEllipsisIcon className="h-6 w-6" />
+          </span>
+          <span>Continue with SMS</span>
         </Link>
       </div>
     </>
