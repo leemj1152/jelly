80e4113555444ac2758a0e0e11aaa2d1b7221692
leemj9812 <dlaudwo087@gmail.com>
2024-04-30 13:48:13 +0900

    home 경로 이동

--- lib/db.ts
+++ lib/db.ts
@@ -2,17 +2,4 @@ import { PrismaClient } from "@prisma/client";
 
 const db = new PrismaClient();
 
-async function test() {
-  const token = await db.sMSToken.create({
-    data: {
-      token: "123141",
-      user: {
-        connect: {
-          id: 12334,
-        },
-      },
-    },
-  });
-  console.log(token);
-}
 export default db;
